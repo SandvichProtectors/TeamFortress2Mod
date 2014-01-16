c023aff7f5b6529922c4c0a29184674b59fcc4d1
Sandvichprotectors <sandvichprotectors@gmail.com>
2014-01-16 12:44:53 +1030

    Fixed some code in the Reference.

--- tf2_common/com/github/sandvichprotectors/teamfortress2/lib/Reference.java
+++ tf2_common/com/github/sandvichprotectors/teamfortress2/lib/Reference.java
@@ -14,6 +14,5 @@ public static final String SERVER_PROXY_CLASS = "com.github.sandvichprotectors.t
     
 public static final String CLIENT_PROXY_CLASS = "com.github.sandvichprotectors.teamfortress2.ClientProxy";
 
-public static final String MinecraftVersion = "1.6.4";
 
 }
